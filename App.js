01b12f108aa8c5555fe8adf86b2bff2807eaaeec
Mehdihussainmalla <mehdihussainmalla@gmail.com>
2022-04-21 23:08:18 +0530

    lang

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React,{useEffect} from 'react'
 import Routes from './src/navigation/Routes'
+import strings from './src/constants/lang'
+
+
 
 const App = () => {
+
+  useEffect(()=>{
+    getLanguage();
+
+  },[])
+  const   getLanguage= async () => {
+    try {
+      const lng = await AsyncStorage.getItem('language')
+      console.log("Lnguage changed", lng)
+      if (!!lng) {
+        strings.setLanguage(lng)
+      } else {
+        strings.setLanguage('en')
+      }
+    } catch (error) {
+      console.log("error occurred in ")
+    }
+  }
   return (
      
     <Routes/>
--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,20 +1,35 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react'
-import { View, Text ,SafeAreaView,TouchableOpacity} from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
 import WrappertContainer from '../../Components/WrapperContainer/'
+import RNRestart from 'react-native-restart'
 import navigationStrings from '../../navigation/navigationStrings';
+import strings, { changeLanguage } from '../../constants/lang';
 
 
-const Login = ({navigation}) => {
+
+const Login = ({ navigation }) => {
+
+  const onchangeLang = (key) => {
+    changeLanguage(key);
+    //RNRestart.Restart();
+
+
+
+  }
   return (
-     
-        <SafeAreaView>
-    <View style={{height:'100%', width:'100%',}}>
-      <TouchableOpacity  onPress={() =>navigation.navigate(navigationStrings.CHOOSE_ACCOUNT)}>
-      <Text style={{color:'black', alignSelf:'center',fontSize:50}}>Login</Text>
-      </TouchableOpacity>
-    </View>
-   
+
+    <SafeAreaView>
+      <View style={{ height: '100%', width: '100%', }}>
+        <TouchableOpacity onPress={() => navigation.navigate(navigationStrings.CHOOSE_ACCOUNT)}>
+          <Text style={{ color: 'black', alignSelf: 'center', fontSize: 50 }}>{strings.LOGIN}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onchangeLang('en')}>   
+     <Text>{strings.URDU}</Text>
+        </TouchableOpacity>
+
+      </View> 
+
     </SafeAreaView>
   )
 }
